74d91aa65bd02cbb60e269d2dd6d62dc602db9b6
mincrmatt12 <mincrmatt12@users.noreply.github.com>
2019-03-22 22:18:42 -0400

    Experimental makeglossaries support

--- textle/steps/tex.py
+++ textle/steps/tex.py
@@ -13,13 +13,14 @@ class TeXStep(Step):
     name = "tex"
 
     def handles_extra_type(self, et):
-        return et == "biblatex"
+        return et in ("biblatex", "glossaries")
 
     def __init__(self, files, subtype, extras, options):
         super().__init__(files, subtype, extras, options)
 
 
         self.has_bib = False
+        self.has_glossary = False
         self.driver = "xetex" if subtype is None else subtype
 
         for x in extras:
@@ -35,6 +36,8 @@ class TeXStep(Step):
                 self.bib_options = extra.options
                 self.bib_source = extra.files[0]
                 self.bib_driver = "biber" if extra.subtype is None else extra.subtype
+            if extra.name == "glossaries":
+                self.has_glossary = True
         
     def get_input_types_valid(self):
         return ["tex"]
@@ -43,29 +46,30 @@ class TeXStep(Step):
         return "pdf"
 
     def get_products(self):
-        if self.has_bib:
-            return super().get_products() + [
-                FileRef(self.input.tag, "bcf", FileUse.GENERATED),
-                FileRef(self.input.tag, "bbl", FileUse.GENERATED),
-            ]
-        else:
-            return super().get_products()
+        return super().get_products() + [
+            FileRef(self.input.tag, "bcf", FileUse.GENERATED),
+            FileRef(self.input.tag, "bbl", FileUse.GENERATED),
+        ] if self.has_bib else [] + [FileRef(self.input.tag, "glo", FileUse.GENERATED)] if self.has_glossary else []
 
     def get_dependencies_for(self, product):
         # Ignores has bib because we can just generate it
 
-        if product.ext == "pdf" and not self.has_bib:
-            return self.files
-        elif product.ext == "pdf":
-            return self.files + [
-                FileRef(self.input.tag, "bbl", FileUse.GENERATED),
-            ]
+        pdf_output = self.files
+        if self.has_bib:
+            pdf_output.append(FileRef(self.input.tag, "bbl", FileUse.GENERATED))
+        if self.has_glossary:
+            pdf_output.append(FileRef(self.input.tag, "glo", FileUse.GENERATED))
+
+        if product.ext == "pdf":
+            return pdf_output
         elif product.ext == "bbl":
             return [
                 FileRef(self.input.tag, "bcf", FileUse.GENERATED),
             ]
         elif product.ext == "bcf":
             return self.files + [self.bib_source]
+        elif product.ext == "glo":
+            return self.files
 
     def solve_inout(self):
         pass
@@ -74,6 +78,7 @@ class TeXStep(Step):
         pdf = self.output
         bcf = FileRef(self.input.tag, "bcf", FileUse.GENERATED)
         bbl = FileRef(self.input.tag, "bbl", FileUse.GENERATED)
+        glo = FileRef(self.input.tag, "glo", FileUse.GENERATED)
 
         if product == pdf:
             return ([self.driver, "-interaction=batchmode", self.input],[MV, FileRef(self.input.tag, "pdf", FileUse.GENERATED), pdf]) 
@@ -81,4 +86,6 @@ class TeXStep(Step):
             return ([self.bib_driver, FileRef(self.input.tag, None, FileUse.GENERATED)],)
         elif product == bcf:
             return ([self.driver, self.input],)
+        elif product == glo:
+            return (["makeglossaries", FileRef(self.input.tag, None, FileUse.GENERATED)],)
 
